d719901635d32b8e9c941109da7606edfe4016be
Perrin <tom.per@orange.fr>
2020-02-20 17:13:03 -0500

    feat(#24): système de payement fonctionnel avec l'envoi de la facture par mail.

--- public/vues/payement/payement.php
+++ public/vues/payement/payement.php
@@ -2,16 +2,30 @@
 
 <?php include "./../includes/header.php" ?>
 
-<form action="charge.php" method="post">
-  <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-          data-key="<?php echo $stripe['publishable_key']; ?>"
-          data-name="Gold Tier"
-          data-description="Access for a year"
-          data-amount="5000"
-          data-currency="cad"
-          data-locale="auto"
-          data-label="Subscribe"></script>
+<button id="checkout-button">Pay</button>
 
-</form>
+<script src="https://js.stripe.com/v3"></script>
+<script>
+var stripe = Stripe('<?php echo $stripe['publishable_key']; ?>');
+
+var checkoutButton = document.querySelector('#checkout-button');
+checkoutButton.addEventListener('click', function () {
+  stripe.redirectToCheckout({
+    items: [{
+      // Define the product and SKU in the Dashboard first, and use the SKU
+      // ID in your client-side code.
+      sku: 'sku_Gltrh8elnSx1XP',
+      quantity: 1
+    }],
+    successUrl: 'http://localhost:3000/public/vues/index.php',
+    cancelUrl: 'http://localhost:3000/public/vues/payement/payement.php'
+  }).then(function (result) {
+  // If `redirectToCheckout` fails due to a browser or network
+  // error, display the localized error message to your customer
+  console.log(result.error.message);
+});
+});
+</script>
 <?php include "./../includes/footer.php" ?>
+
 
